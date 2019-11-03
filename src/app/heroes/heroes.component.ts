68a29f036048d7455124f36726f8f7b8ab0278ea
Alfonso Jimenez <djalfo18@gmail.com>
2019-11-03 16:07:19 -0500

    Creation of Hero and Message services and their binding with heroes and messages components using Angular Dependency Injection and RxJS Observable library.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,7 +6,8 @@ import { Work } from "../work";
 import { Connections } from "../connections";
 import { Images } from "../images";
 import { Hero } from "../hero";
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 
 @Component({
@@ -16,17 +17,23 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
